bc5a1e6628ca189e79b0cbe846b62d0c25790dbe
Marcial Rico <mrico@clarcat.com>
2022-09-29 14:13:52 +0200

    Second upload

--- force-app/main/default/lwc/calculateFlightLWC/calculateFlightLWC.js
+++ force-app/main/default/lwc/calculateFlightLWC/calculateFlightLWC.js
@@ -11,6 +11,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 import getIATACodes from '@salesforce/apex/CalculateFlightController.retrieveIATACodes';
 import getCreateFlight from '@salesforce/apex/CalculateFlightController.registerFlight';
 
+//toast messages
 const ERRORBLANK = 'Please, select a departure and an arrival airport to calculate flight distance';
 const ERROR = 'Error';
 const SUCCESS = 'Success';
@@ -19,21 +20,21 @@ const ERRORCREATING = 'A problem occurred calculating flight distance';
 
 export default class CalculateFlightLWC extends LightningElement {
     departureCode = '';
-    departureCodeStatic = '';
+    departureCodeStatic = ''; //used to show the code after the flight has been created
     arrivalCode = '';
-    arrivalCodeStatic = '';
-    departureName = '';
-    arrivalName = '';
+    arrivalCodeStatic = ''; //used to show the code after the flight has been created
+    departureName = ''; //used to show the full airport name after the flight has been created
+    arrivalName = ''; //used to show the full airport name after the flight has been created
     distance = 0.0;
     options = [];
-    loaded = false;
-    calculated = false;
+    loaded = false; //if true, it shows the IATA Code's picklists
+    calculated = false; //if true, it shows the flight information
 
-    @wire(getIATACodes)
+    @wire(getIATACodes) //wired function to retrieve IATA Codes 
     airports({error, data}){
         if (data) {
             for (let i = 0; i < data.length; i++) {
-                this.options.push({ label: data[i], value: data[i] });
+                this.options.push({ label: data[i], value: data[i] }); //push data to both picklists
             }
             this.loaded = true;
         } else if (error) {
@@ -43,23 +44,27 @@ export default class CalculateFlightLWC extends LightningElement {
         }
     }
 
+    // handled the change in the option selected for the departure airport IATA code
     handleChangeDeparture(event) {
         this.departureCode = event.detail.value;
         console.log(this.departureCode);
     }
 
+    // handled the change in the option selected for the arrival airport IATA code
     handleChangeArrival(event) {
         this.arrivalCode = event.detail.value;
         console.log(this.arrivalCode);
     }
 
+    // handled the creation of the flight given two IATA codes
     handleCreateFlight(){
         if(this.arrivalCode == '' || this.departureCode == ''){
             this.showToast(ERROR,ERRORBLANK);
         } else{
-            getCreateFlight({departureCode:this.departureCode, arrivalCode:this.arrivalCode})
+            getCreateFlight({departureCode:this.departureCode, arrivalCode:this.arrivalCode}) //call apex method
             .then(result => {
-                if(result != null){
+                //if created, we transfer information from the object to show on the component
+                if(result != null){ 
                     this.showToast(SUCCESS,SUCCESSMSG);
                     this.distance = result.Distance__c;
                     this.departureName = result.Departure_airport_name__c;
@@ -67,7 +72,9 @@ export default class CalculateFlightLWC extends LightningElement {
                     this.arrivalName = result.Arrival_airport_name__c;
                     this.arrivalCodeStatic = this.arrivalCode;
                     this.calculated = true;
-                } else{
+                } 
+                // else, we show an error toast
+                else{
                     this.showToast(ERROR,ERRORCREATING);
                 }
             })
@@ -77,6 +84,7 @@ export default class CalculateFlightLWC extends LightningElement {
         }
     }
 
+    //funtion that shows toasts with diverse information
     showToast(title, message) {
         const event = new ShowToastEvent({
             title: title,
